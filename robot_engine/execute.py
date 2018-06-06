b98de3797ecf78893af226fbfeb8f821212b9a05
daniel.liu <daniel.liu@derbysofot.com>
2018-06-06 16:02:26 +0800

    change port

--- robot_engine/execute.py
+++ robot_engine/execute.py
@@ -59,7 +59,7 @@ def run_script(request, project, test_id):
     finally:
         os.chdir(opath)
         utility.kill(pid)
-        utility.remove_file(reportpath)
-        utility.remove_file(script_path_zip)
+        # utility.remove_file(reportpath)
+        # utility.remove_file(script_path_zip)
         # utility.remove_file(script_path)
         return reportpath_zip
