a6dfb6b9b11214aa5718c83de73804ceb272ebf7
daniel.liu <daniel.liu@derbysofot.com>
2018-07-23 15:51:01 +0800

    change port

--- doraemon_install.sh
+++ doraemon_install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-pip install -U doraemon
-pip install -U bestwestern-storage-client
-pip install -U dstorage-client
-pip install -U dswitch3-client
-pip install -U dswitch3-server
-pip install -U dswitch3-server-match
-pip install -U jetty-runner
-pip install -U pixiu-test-tool
-pip install -U shop-storage-save
-pip install -U synchronizer-client-1
-pip install -U synchronizer-client-2
-pip install -U synchronizer-server-1
-pip install -U synchronizer-server-2
+pip install -U doraemon --trusted-host 172.27.0.172
+pip install -U bestwestern-storage-client --trusted-host 172.27.0.172
+pip install -U dstorage-client --trusted-host 172.27.0.172
+pip install -U dswitch3-client --trusted-host 172.27.0.172
+pip install -U dswitch3-server --trusted-host 172.27.0.172
+pip install -U dswitch3-server-match --trusted-host 172.27.0.172
+pip install -U jetty-runner --trusted-host 172.27.0.172
+pip install -U pixiu-test-tool --trusted-host 172.27.0.172
+pip install -U shop-storage-save --trusted-host 172.27.0.172
+pip install -U synchronizer-client-1 --trusted-host 172.27.0.172
+pip install -U synchronizer-client-2 --trusted-host 172.27.0.172
+pip install -U synchronizer-server-1 --trusted-host 172.27.0.172
+pip install -U synchronizer-server-2 --trusted-host 172.27.0.172
