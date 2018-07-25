3cf551ffeb783827cfb189f1ac568373084b400f
daniel.liu <daniel.liu@derbysofot.com>
2018-07-25 15:20:35 +0800

    update job

---
+++ Baymax_Node/jobs.py
@@ -0,0 +1,27 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+import os
+import subprocess
+import shlex
+import sys
+
+mswindows = (sys.platform == "win32")
+
+scheduler = BackgroundScheduler()
+
+scheduler.start()
+
+
+def execute_shell():
+    if not mswindows:
+        Base_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        Shell_DIR = os.path.join(Base_DIR, "shell")
+        for dirpath, dirnames, filenames in os.walk(Shell_DIR):
+            for filename in filenames:
+                f = os.path.join(dirpath, filename)
+                command = "sh {}".format(f)
+                log = open(os.path.join(Shell_DIR, "{}.log".format(filename)), 'w')
+                subprocess.call(shlex.split(command), stdout=log, stderr=subprocess.STDOUT)
+
+
+scheduler.add_job(execute_shell, 'interval', minutes=5)
+print("Scheduler started!")
