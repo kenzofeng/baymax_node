879dc3064eb3606247a18f9c4062c8dd97bc35c7
daniel.liu <daniel.liu@derbysofot.com>
2018-07-25 15:41:43 +0800

    update job

--- Baymax_Node/jobs.py
+++ Baymax_Node/jobs.py
@@ -5,20 +5,19 @@ import shlex
 
 scheduler = BackgroundScheduler()
 scheduler.start()
+logpath = '/usr/local/logs'
 
 
 def execute_shell():
-    print 'execute_shell'
     Base_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     Shell_DIR = os.path.join(Base_DIR, "shell")
     for dirpath, dirnames, filenames in os.walk(Shell_DIR):
         for filename in filenames:
             f = os.path.join(dirpath, filename)
             command = "sh {}".format(f)
-            print command
-            log = open(os.path.join(Shell_DIR, "{}.log".format(filename)), 'w')
+            log = open(os.path.join(logpath, "{}.log".format(filename)), 'w')
             subprocess.call(shlex.split(command), stdout=log, stderr=subprocess.STDOUT)
 
 
-scheduler.add_job(execute_shell, 'interval', minutes=1)
+scheduler.add_job(execute_shell, 'interval', minutes=5)
 print("Scheduler started!")
