5d17a84c975acb0acecf246bc272bbaa8ecad2a5
daniel.liu <daniel.liu@derbysofot.com>
2018-08-22 13:51:18 +0800

    update shell

--- shell/doraemon_install.sh
+++ shell/doraemon_install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-pip install -U doraemon --trusted-host 172.27.0.172
-pip install -U bestwestern-storage-client --trusted-host 172.27.0.172
-pip install -U dstorage-client --trusted-host 172.27.0.172
-pip install -U dswitch3-client --trusted-host 172.27.0.172
-pip install -U dswitch3-server --trusted-host 172.27.0.172
-pip install -U dswitch3-server-match --trusted-host 172.27.0.172
-pip install -U jetty-runner --trusted-host 172.27.0.172
-pip install -U pixiu-test-tool --trusted-host 172.27.0.172
-pip install -U shop-storage-save --trusted-host 172.27.0.172
-pip install -U synchronizer-client-1 --trusted-host 172.27.0.172
-pip install -U synchronizer-client-2 --trusted-host 172.27.0.172
-pip install -U synchronizer-server-1 --trusted-host 172.27.0.172
-pip install -U synchronizer-server-2 --trusted-host 172.27.0.172
+pip install -U doraemon -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
+pip install -U bestwestern-storage-client -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
+pip install -U dstorage-client -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
+pip install -U dswitch3-client -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
+pip install -U dswitch3-server -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
+pip install -U dswitch3-server-match -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
+pip install -U jetty-runner -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
+pip install -U pixiu-test-tool -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
+pip install -U shop-storage-save -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
+pip install -U synchronizer-client-1 -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
+pip install -U synchronizer-client-2 -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
+pip install -U synchronizer-server-1 -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
+pip install -U synchronizer-server-2 -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
