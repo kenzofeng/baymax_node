181916bc477a145a2000ecf0449dfcd3859ab427
daniel.liu <daniel.liu@derbysofot.com>
2018-08-23 15:05:03 +0800

    update shell

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sed -i 's/#releasever=latest/releasever=latest/g' /etc/yum.conf
-yum install gcc -y
+yum install gcc libffi-devel python-devel openssl-devel -y
+pip install -U setuptools
 pip install -r requirements.txt
 sh  shell/doraemon_install.sh
 ln -s /usr/local/redis/bin/redis-server /usr/bin/redis-server
