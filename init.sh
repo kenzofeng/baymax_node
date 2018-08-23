01a9fe8fa01cc7f8d68bb55989289d28dd5cd2c7
daniel.liu <daniel.liu@derbysofot.com>
2018-08-23 15:14:52 +0800

    update shell

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sed -i 's/#releasever=latest/releasever=latest/g' /etc/yum.conf
-yum install gcc libffi-devel python-devel openssl-devel -y
+yum install gcc libffi-devel python-devel openssl-devel libxml2-devel libxslt-devel -y
 pip install -U setuptools
 pip install -r requirements.txt
 sh  shell/doraemon_install.sh
