fd942a6ffba8f8dedee566cc0f23f92cede8c5eb
daniel.liu <daniel.liu@derbysofot.com>
2018-08-28 10:51:10 +0800

    add log function

--- robot_engine/execute.py
+++ robot_engine/execute.py
@@ -2,7 +2,7 @@ import logging
 import os
 import shlex
 import subprocess
-
+import threading
 import requests
 
 import utility
@@ -12,13 +12,23 @@ from node import env
 logger = logging.getLogger('django')
 
 
+def get_robot_log(robot,mylog):
+    while True:
+        log = robot.stdout.readline()
+        mylog.robot_info(log.replace('\r\n', ''))
+        if 'Report:' in log and 'report.html' in log:
+            break
+        if robot.poll() is not None:
+            break
+
+
 def run_script(request, project, test_id):
     opath = os.getcwd()
     pid = 0
     reportpath_zip = ""
     try:
         mylog = Mylogger(os.path.join(env.log, test_id))
-        mylog.robot_info("ip:%s \nid:%s \nlog:" % (requests.get('http://ip.42.pl/raw').text, test_id))
+        mylog.robot_info("ip:%s id:%s" % (requests.get('http://ip.42.pl/raw').text, test_id))
         script = request.FILES['script']
         script_path_zip = os.path.join(env.test, request.POST['filename'])
         script_path = os.path.join(env.test, project)
@@ -42,14 +52,9 @@ def run_script(request, project, test_id):
         mylog.robot_info(command)
         robot = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         pid = robot.pid
-
-        while True:
-            log = robot.stdout.readline()
-            mylog.robot_info(log.replace('\r\n', ''))
-            if 'Report:' in log and 'report.html' in log:
-                break
-            if robot.poll() is not None:
-                break
+        r = threading.Thread(target=get_robot_log, args=(robot,mylog))
+        r.start()
+        r.join()
         try:
             if robot is not None:
                 robot.terminate()
@@ -64,7 +69,6 @@ def run_script(request, project, test_id):
         os.chdir(opath)
         utility.kill(pid)
         utility.remove_file(reportpath)
-        utility.remove_file(reportpath_zip)
         utility.remove_file(script_path_zip)
         # utility.remove_file(script_path)
         return reportpath_zip
