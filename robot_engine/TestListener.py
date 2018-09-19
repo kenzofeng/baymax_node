b28f4f4c0b57bca8f4f9dca8df4cb111bcd802ea
daniel.liu <daniel.liu@derbysofot.com>
2018-09-19 13:38:22 +0800

    add listener

--- robot_engine/TestListener.py
+++ robot_engine/TestListener.py
@@ -1,25 +1,23 @@
-from robot.output import LOGGER
-from robot.output.loggerhelper import Message
-
+from robot.api import logger
 
 class TestListener:
     ROBOT_LISTENER_API_VERSION = 2
 
     def __init__(self):
-        LOGGER.log_message(Message('Hello, world!', 'INFO'))
+        logger.console('Hello, world!', 'INFO')
 
     def start_keyword(self, name, attrs):
-        LOGGER.log_message(Message("%s '%s'\n" % (name, attrs['args']), 'INFO'))
+        logger.console("%s '%s'\n" % (name, attrs['args']), 'INFO')
 
     def end_keyword(self, name, attrs):
-        LOGGER.log_message(Message("%s '%s'\n" % (name, attrs['args']), 'INFO'))
+        logger.console("%s '%s'\n" % (name, attrs['args']), 'INFO')
 
     def start_suite(self, name, attrs):
-        LOGGER.log_message(Message("%s '%s'\n" % (name, attrs['doc']), 'INFO'))
+        logger.console("%s '%s'\n" % (name, attrs['doc']), 'INFO')
 
     def start_test(self, name, attrs):
         tags = ' '.join(attrs['tags'])
-        LOGGER.log_message(Message("- %s '%s' [ %s ] :: " % (name, attrs['doc'], tags), 'INFO'))
+        logger.console("- %s '%s' [ %s ] :: " % (name, attrs['doc'], tags), 'INFO')
 
         # def end_test(self, name, attrs):
         #     if attrs['status'] == 'PASS':
@@ -28,4 +26,4 @@ class TestListener:
         #         self.logger.info('FAIL: %s\n' % attrs['message'])
 
     def end_suite(self, name, attrs):
-        LOGGER.log_message(Message('%s\n%s\n' % (attrs['status'], attrs['message']), 'INFO'))
+        logger.console('%s\n%s\n' % (attrs['status'], attrs['message']), 'INFO')
