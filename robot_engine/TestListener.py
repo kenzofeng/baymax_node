a45985415d79dcac695ab9a88e00582067457f15
daniel.liu <daniel.liu@derbysofot.com>
2018-09-19 12:23:10 +0800

    add listener prot

---
+++ robot_engine/TestListener.py
@@ -0,0 +1,30 @@
+class TestListener:
+    ROBOT_LISTENER_API_VERSION = 2
+
+    def __init__(self, path):
+        self.outfile = open(path, 'w')
+
+    def start_keyword(self, name, attrs):
+        self.outfile.write("%s '%s'\n" % (name, attrs['args']))
+
+    def end_keyword(self, name, attrs):
+        self.outfile.write("%s '%s'\n" % (name, attrs['args']))
+
+    def start_suite(self, name, attrs):
+        self.outfile.write("%s '%s'\n" % (name, attrs['doc']))
+
+    def start_test(self, name, attrs):
+        tags = ' '.join(attrs['tags'])
+        self.outfile.write("- %s '%s' [ %s ] :: " % (name, attrs['doc'], tags))
+
+    def end_test(self, name, attrs):
+        if attrs['status'] == 'PASS':
+            self.outfile.write('PASS\n')
+        else:
+            self.outfile.write('FAIL: %s\n' % attrs['message'])
+
+    def end_suite(self, name, attrs):
+        self.outfile.write('%s\n%s\n' % (attrs['status'], attrs['message']))
+
+    def close(self):
+        self.outfile.close()
