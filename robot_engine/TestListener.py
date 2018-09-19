9d4a96d9ba8d3d05ce6a714b265de10a75588315
daniel.liu <daniel.liu@derbysofot.com>
2018-09-19 13:19:32 +0800

    add listener

--- robot_engine/TestListener.py
+++ robot_engine/TestListener.py
@@ -1,33 +1,32 @@
-import logging
+
 
 
 class TestListener:
     ROBOT_LISTENER_API_VERSION = 2
 
-    def __init__(self, path):
-        self.logger = logging.getLogger('listener')
-        self.logger.setLevel(logging.INFO)
-        self.logger.addFilter(logging.FileHandler(path))
-        self.logger.info('111111111111111111')
+    def __init__(self,path):
+        self.outfile = open(path, 'w')
 
     def start_keyword(self, name, attrs):
-        self.logger.info("%s '%s'\n" % (name, attrs['args']))
+        self.outfile.write("%s '%s'\n" % (name, attrs['args']))
 
     def end_keyword(self, name, attrs):
-        self.logger.info("%s '%s'\n" % (name, attrs['args']))
+        self.outfile.write("%s '%s'\n" % (name, attrs['args']))
 
     def start_suite(self, name, attrs):
-        self.logger.info("%s '%s'\n" % (name, attrs['doc']))
+        self.outfile.write("%s '%s'\n" % (name, attrs['doc']))
 
     def start_test(self, name, attrs):
         tags = ' '.join(attrs['tags'])
-        self.logger.info("- %s '%s' [ %s ] :: " % (name, attrs['doc'], tags))
+        self.outfile.write("- %s '%s' [ %s ] :: " % (name, attrs['doc'], tags))
 
     def end_test(self, name, attrs):
         if attrs['status'] == 'PASS':
-            self.logger.info('PASS\n')
+            self.outfile.write('PASS\n')
         else:
-            self.logger.info('FAIL: %s\n' % attrs['message'])
+            self.outfile.write('FAIL: %s\n' % attrs['message'])
 
     def end_suite(self, name, attrs):
-        self.logger.info('%s\n%s\n' % (attrs['status'], attrs['message']))
+        self.outfile.write('%s\n%s\n' % (attrs['status'], attrs['message']))
+
+
