53ad14c4f162537b3a86d54cade030fa5f16c275
daniel.liu <daniel.liu@derbysofot.com>
2018-09-19 13:07:39 +0800

    add listener

--- robot_engine/TestListener.py
+++ robot_engine/TestListener.py
@@ -1,31 +1,32 @@
 import logging
 
-logger =logging.getLogger('robot')
+
 
 class TestListener:
     ROBOT_LISTENER_API_VERSION = 2
 
     def __init__(self):
-        logger.debug('111111111111111111')
+        self.logger = logging.getLogger('robot')
+        self.logger.debug('111111111111111111')
 
     def start_keyword(self, name, attrs):
-        logger.debug("%s '%s'\n" % (name, attrs['args']))
+        self.logger.debug("%s '%s'\n" % (name, attrs['args']))
 
     def end_keyword(self, name, attrs):
-        logger.debug("%s '%s'\n" % (name, attrs['args']))
+        self.logger.debug("%s '%s'\n" % (name, attrs['args']))
 
     def start_suite(self, name, attrs):
-        logger.debug("%s '%s'\n" % (name, attrs['doc']))
+        self.logger.debug("%s '%s'\n" % (name, attrs['doc']))
 
     def start_test(self, name, attrs):
         tags = ' '.join(attrs['tags'])
-        logger.debug("- %s '%s' [ %s ] :: " % (name, attrs['doc'], tags))
+        self.logger.debug("- %s '%s' [ %s ] :: " % (name, attrs['doc'], tags))
 
     def end_test(self, name, attrs):
         if attrs['status'] == 'PASS':
-            logger.debug('PASS\n')
+            self.logger.debug('PASS\n')
         else:
-            logger.debug('FAIL: %s\n' % attrs['message'])
+            self.logger.debug('FAIL: %s\n' % attrs['message'])
 
     def end_suite(self, name, attrs):
-        logger.debug('%s\n%s\n' % (attrs['status'], attrs['message']))
+        self.logger.debug('%s\n%s\n' % (attrs['status'], attrs['message']))
