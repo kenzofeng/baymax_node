00f1fb4d06fde82e370fba2aeeff9fa2929802b0
daniel.liu <daniel.liu@derbysofot.com>
2018-09-19 13:33:38 +0800

    add listener

--- robot_engine/TestListener.py
+++ robot_engine/TestListener.py
@@ -1,36 +1,31 @@
-import logging
-import os
-import tempfile
+from robot.output import LOGGER
+from robot.output.loggerhelper import Message
 
 
 class TestListener:
     ROBOT_LISTENER_API_VERSION = 2
 
     def __init__(self):
-        self.logger = logging.getLogger('listern')
-        rf = logging.FileHandler(os.path.join(tempfile.gettempdir(), 'listener.txt'))
-        self.logger.setLevel(logging.INFO)
-        self.logger.addFilter(rf)
-        self.logger.info('111111111111111111')
+        LOGGER.log_message(Message('Hello, world!', 'INFO'))
 
     def start_keyword(self, name, attrs):
-        self.logger.info("%s '%s'\n" % (name, attrs['args']))
+        LOGGER.log_message(Message("%s '%s'\n" % (name, attrs['args']), 'INFO'))
 
     def end_keyword(self, name, attrs):
-        self.logger.info("%s '%s'\n" % (name, attrs['args']))
+        LOGGER.log_message(Message("%s '%s'\n" % (name, attrs['args']), 'INFO'))
 
     def start_suite(self, name, attrs):
-        self.logger.info("%s '%s'\n" % (name, attrs['doc']))
+        LOGGER.log_message(Message("%s '%s'\n" % (name, attrs['doc']), 'INFO'))
 
     def start_test(self, name, attrs):
         tags = ' '.join(attrs['tags'])
-        self.logger.info("- %s '%s' [ %s ] :: " % (name, attrs['doc'], tags))
+        LOGGER.log_message(Message("- %s '%s' [ %s ] :: " % (name, attrs['doc'], tags), 'INFO'))
 
-    def end_test(self, name, attrs):
-        if attrs['status'] == 'PASS':
-            self.logger.info('PASS\n')
-        else:
-            self.logger.info('FAIL: %s\n' % attrs['message'])
+        # def end_test(self, name, attrs):
+        #     if attrs['status'] == 'PASS':
+        #         self.logger.info('PASS\n')
+        #     else:
+        #         self.logger.info('FAIL: %s\n' % attrs['message'])
 
     def end_suite(self, name, attrs):
-        self.logger.info('%s\n%s\n' % (attrs['status'], attrs['message']))
+        LOGGER.log_message(Message('%s\n%s\n' % (attrs['status'], attrs['message']), 'INFO'))
