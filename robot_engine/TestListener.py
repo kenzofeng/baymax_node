32fee6ecfcfafea5ac8b1444173c3576e5c49953
daniel.liu <daniel.liu@derbysofot.com>
2018-09-19 13:28:01 +0800

    add listener

--- robot_engine/TestListener.py
+++ robot_engine/TestListener.py
@@ -1,32 +1,36 @@
-
+import logging
+import os
+import tempfile
 
 
 class TestListener:
     ROBOT_LISTENER_API_VERSION = 2
 
-    def __init__(self,path):
-        self.outfile = open(path, 'w')
+    def __init__(self):
+        self.logger = logging.getLogger('listern')
+        rf = logging.FileHandler(os.path.join(tempfile.gettempdir(), 'listener.txt'))
+        self.logger.setLevel(logging.INFO)
+        self.logger.addFilter(rf)
+        self.logger.info('111111111111111111')
 
     def start_keyword(self, name, attrs):
-        self.outfile.write("%s '%s'\n" % (name, attrs['args']))
+        self.logger.info("%s '%s'\n" % (name, attrs['args']))
 
     def end_keyword(self, name, attrs):
-        self.outfile.write("%s '%s'\n" % (name, attrs['args']))
+        self.logger.info("%s '%s'\n" % (name, attrs['args']))
 
     def start_suite(self, name, attrs):
-        self.outfile.write("%s '%s'\n" % (name, attrs['doc']))
+        self.logger.info("%s '%s'\n" % (name, attrs['doc']))
 
     def start_test(self, name, attrs):
         tags = ' '.join(attrs['tags'])
-        self.outfile.write("- %s '%s' [ %s ] :: " % (name, attrs['doc'], tags))
+        self.logger.info("- %s '%s' [ %s ] :: " % (name, attrs['doc'], tags))
 
     def end_test(self, name, attrs):
         if attrs['status'] == 'PASS':
-            self.outfile.write('PASS\n')
+            self.logger.info('PASS\n')
         else:
-            self.outfile.write('FAIL: %s\n' % attrs['message'])
+            self.logger.info('FAIL: %s\n' % attrs['message'])
 
     def end_suite(self, name, attrs):
-        self.outfile.write('%s\n%s\n' % (attrs['status'], attrs['message']))
-
-
+        self.logger.info('%s\n%s\n' % (attrs['status'], attrs['message']))
--- robot_engine/execute.py
+++ robot_engine/execute.py
@@ -69,9 +69,8 @@ def run_script(request, project, test_id):
         os.chdir(script_path)
         os.system('chmod 777 -R *')
         if os.path.exists(argfile):
-            command = "python -m robot.run --argumentfile {} --listener {}:{} --outputdir {}  {}".format(argfile,
+            command = "python -m robot.run --argumentfile {} --listener {} --outputdir {}  {}".format(argfile,
                                                                                                          listener,
-                                                                                                         mylog.path,
                                                                                                          reportpath,
                                                                                                          script_path)
         else:
