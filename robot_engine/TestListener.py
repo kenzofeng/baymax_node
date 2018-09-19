31dcd4f4fcbe27998f2162ab9505625b8faadebb
daniel.liu <daniel.liu@derbysofot.com>
2018-09-19 12:43:29 +0800

    add listener

--- robot_engine/TestListener.py
+++ robot_engine/TestListener.py
@@ -7,23 +7,23 @@ class TestListener:
         logger.info('111111111111111')
 
     def start_keyword(self, name, attrs):
-        logger.log_message("%s '%s'\n" % (name, attrs['args']))
+        logger.info("%s '%s'\n" % (name, attrs['args']))
 
     def end_keyword(self, name, attrs):
-        logger.log_message("%s '%s'\n" % (name, attrs['args']))
+        logger.info("%s '%s'\n" % (name, attrs['args']))
 
     def start_suite(self, name, attrs):
-        logger.log_message("%s '%s'\n" % (name, attrs['doc']))
+        logger.info("%s '%s'\n" % (name, attrs['doc']))
 
     def start_test(self, name, attrs):
         tags = ' '.join(attrs['tags'])
-        logger.log_message("- %s '%s' [ %s ] :: " % (name, attrs['doc'], tags))
+        logger.info("- %s '%s' [ %s ] :: " % (name, attrs['doc'], tags))
 
     def end_test(self, name, attrs):
         if attrs['status'] == 'PASS':
-            logger.log_message('PASS\n')
+            logger.info('PASS\n')
         else:
-            logger.log_message('FAIL: %s\n' % attrs['message'])
+            logger.info('FAIL: %s\n' % attrs['message'])
 
     def end_suite(self, name, attrs):
-        logger.log_message('%s\n%s\n' % (attrs['status'], attrs['message']))
+        logger.info('%s\n%s\n' % (attrs['status'], attrs['message']))
