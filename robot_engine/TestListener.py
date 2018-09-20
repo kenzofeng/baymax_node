757be8ff17bda0cb133e7c2de3b4ccc6b65bd6a5
daniel.liu <daniel.liu@derbysofot.com>
2018-09-20 14:25:45 +0800

    add listener

--- robot_engine/TestListener.py
+++ robot_engine/TestListener.py
@@ -5,7 +5,7 @@ class TestListener:
     ROBOT_LISTENER_API_VERSION = 2
 
     def start_keyword(self, name, attrs):
-        logger.console('Start Keyword:{}.{}'.format(name, " ".join(attrs['args'])))
+        logger.console('Keyword:{}.{}'.format(name, " ".join(attrs['args'])))
 
     def start_suite(self, name, attrs):
         logger.console('Start Suite:{}'.format(name))
