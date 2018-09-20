03268f499195c77f25e34ef9b1522189b707eaa4
daniel.liu <daniel.liu@derbysofot.com>
2018-09-20 14:20:20 +0800

    add listener

--- robot_engine/TestListener.py
+++ robot_engine/TestListener.py
@@ -5,29 +5,7 @@ class TestListener:
     ROBOT_LISTENER_API_VERSION = 2
 
     def start_keyword(self, name, attrs):
-        logger.console('start_keyword')
-        logger.console("%s '%s'\n" % (name, attrs['args']))
-
-
-    def end_keyword(self, name, attrs):
-        logger.console('end_keyword')
-        logger.console("%s '%s'\n" % (name, attrs['args']))
+        logger.console('Start Keyword:{}.{}'.format(name, " ".join(attrs['args'])))
 
     def start_suite(self, name, attrs):
-        logger.console('start_suite')
-        logger.console("%s '%s'\n" % (name, attrs['doc']))
-
-    def start_test(self, name, attrs):
-        logger.console('start_test')
-        tags = ' '.join(attrs['tags'])
-        logger.console("- %s '%s' [ %s ] :: " % (name, attrs['doc'], tags))
-
-        # def end_test(self, name, attrs):
-        #     if attrs['status'] == 'PASS':
-        #         self.logger.info('PASS\n')
-        #     else:
-        #         self.logger.info('FAIL: %s\n' % attrs['message'])
-
-    def end_suite(self, name, attrs):
-        logger.console('end_suite')
-        logger.console('%s\n%s\n' % (attrs['status'], attrs['message']))
+        logger.console('Start Suite:{}'.format(name))
