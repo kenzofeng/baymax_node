7612ebaea037b43c2869f9011ff2c34cae9464e0
daniel.liu <daniel.liu@derbysofot.com>
2018-09-20 14:07:32 +0800

    add listener

--- robot_engine/TestListener.py
+++ robot_engine/TestListener.py
@@ -4,19 +4,21 @@ from robot.api import logger
 class TestListener:
     ROBOT_LISTENER_API_VERSION = 2
 
-    def __init__(self):
-        logger.console('Hello, world!')
-
     def start_keyword(self, name, attrs):
+        logger.console('start_keyword')
         logger.console("%s '%s'\n" % (name, attrs['args']))
 
+
     def end_keyword(self, name, attrs):
+        logger.console('end_keyword')
         logger.console("%s '%s'\n" % (name, attrs['args']))
 
     def start_suite(self, name, attrs):
+        logger.console('start_suite')
         logger.console("%s '%s'\n" % (name, attrs['doc']))
 
     def start_test(self, name, attrs):
+        logger.console('start_test')
         tags = ' '.join(attrs['tags'])
         logger.console("- %s '%s' [ %s ] :: " % (name, attrs['doc'], tags))
 
@@ -27,4 +29,5 @@ class TestListener:
         #         self.logger.info('FAIL: %s\n' % attrs['message'])
 
     def end_suite(self, name, attrs):
+        logger.console('end_suite')
         logger.console('%s\n%s\n' % (attrs['status'], attrs['message']))
