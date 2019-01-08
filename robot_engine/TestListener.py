50f513dfe15c55da7abf4a95c3e08db1b0bbebbd
daniel.liu <daniel.liu@derbysofot.com>
2019-01-08 14:12:04 +0800

    add listener in execute

--- robot_engine/TestListener.py
+++ robot_engine/TestListener.py
@@ -5,10 +5,13 @@ class TestListener:
     ROBOT_LISTENER_API_VERSION = 2
 
     def start_keyword(self, name, attrs):
-        logger.console('Start Keyword:{}.{}'.format(name, attrs['args'][0]))
+        logger.console('Start Keyword:{}.{}'.format(name, attrs['args']))
 
-    def end_keyword(self, name, attrs):
-        logger.console('End Keyword:{}.{}'.format(name, attrs['args'][0]))
+    def start_test(self, name, attrs):
+        logger.console('Start Keyword:{}.{}'.format(name))
 
-    # def start_suite(self, name, attrs):
-    #     logger.console('Start Suite:{}'.format(name))
+    # def end_keyword(self, name, attrs):
+    #     logger.console('End Keyword:{}.{}'.format(name, attrs['args']))
+
+    def start_suite(self, name, attrs):
+        logger.console('Start Suite:{}'.format(name))
