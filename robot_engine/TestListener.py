1006fa885a06bd939dc01d2bed57fb4199c07323
daniel.liu <daniel.liu@derbysofot.com>
2019-01-08 14:43:27 +0800

    update listener in execute

--- robot_engine/TestListener.py
+++ robot_engine/TestListener.py
@@ -8,10 +8,7 @@ class TestListener:
         logger.console('Start Keyword:{}.{}'.format(name, attrs['args']))
 
     def start_test(self, name, attrs):
-        logger.console('Start Keyword:{}.{}'.format(name))
-
-    # def end_keyword(self, name, attrs):
-    #     logger.console('End Keyword:{}.{}'.format(name, attrs['args']))
+        logger.console('Start Test:{}.{}'.format(name))
 
     def start_suite(self, name, attrs):
         logger.console('Start Suite:{}'.format(name))
