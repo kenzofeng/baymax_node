68def76fa36739738f9c97ef9bac353ea3952025
kenzo <416169126@qq.com>
2019-02-13 13:25:10 +0800

    update shell

--- shell/doraemon_install.sh
+++ shell/doraemon_install.sh
@@ -1,17 +1,2 @@
 #!/usr/bin/env bash
 pip install -U doraemon -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
-pip install -U content-server -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
-pip install -U bestwestern-storage-client -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
-pip install -U dstorage-client -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
-pip install -U dstorage-replication-client -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
-pip install -U dswitch3-client -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
-pip install -U dswitch3-server -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
-pip install -U dswitch3-server-match -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
-pip install -U jetty-runner -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
-pip install -U pixiu-test-tool -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
-pip install -U shop-storage-save -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
-pip install -U synchronizer-client-1 -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
-pip install -U synchronizer-client-2 -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
-pip install -U synchronizer-server-1 -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
-pip install -U synchronizer-server-2 -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
-pip install -U protocol_proxy -i http://172.27.0.172:8080/simple  --trusted-host 172.27.0.172
