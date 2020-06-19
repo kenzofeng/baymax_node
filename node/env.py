32b87c7c7ac3ca11904a646a12f8f4e884fd3261
feng <durd8WRIG-paiy>
2020-06-19 15:25:57 +0800

    fix rmtree bug

--- node/env.py
+++ node/env.py
@@ -18,19 +18,22 @@ debug = '/usr/local/logs'
 log = os.path.join(BASE_DIR, project, 'log')
 tmp = os.path.join(BASE_DIR, project, 'tmp')
 
-shutil.rmtree(test)
-shutil.rmtree(report)
-shutil.rmtree(log)
-shutil.rmtree(tmp)
-
 if not os.path.exists(test):
     os.makedirs(test)
+else:
+    shutil.rmtree(test)
 if not os.path.exists(report):
     os.makedirs(report)
+else:
+    shutil.rmtree(report)
 if not os.path.exists(log):
     os.makedirs(log)
+else:
+    shutil.rmtree(log)
 if not os.path.exists(tmp):
     os.makedirs(tmp)
+else:
+    shutil.rmtree(tmp)
 
 log_html = 'log.html'
 report_html = 'report.html'
